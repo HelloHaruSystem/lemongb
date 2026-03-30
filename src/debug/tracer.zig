7412e9dc740cf3551f72eaed0c8ec5f4e6160bc4
Halfdan Sand Christensen <halfdansc@proton.me>
2026-03-30 18:17:52 +0200

    first take on the logger and trace for gameboy doctor

--- src/debug/tracer.zig
+++ src/debug/tracer.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
+const state = @import("../core/cpu.zig").CpuState;
 
-pub const Tracer = struct {
-    pub fn Trace(self: Tracer, writer: *std.Io.Writer) !void {
-        _ = self;
-        try writer.print("A:{d} F:{d} B:{d} C:{d} D:{d} E:{d} H:{d} L:{d} SP:{d} PC:{d} PCMEM:{d}\n", .{});
-    }
-};
+pub fn trace(writer: *std.Io.Writer, cpu_state: state) !void {
+    try writer.print("A:{X:0>2} F:{X:0>2} B:{X:0>2} C:{X:0>2} D:{X:0>2} E:{X:0>2} H:{X:0>2} L:{X:0>2} SP:{X:0>4} PC:{X:0>4} PCMEM:{X:0>2},{X:0>2},{X:0>2},{X:0>2}\n", .{
+        cpu_state.a,
+        cpu_state.f,
+        cpu_state.b,
+        cpu_state.c,
+        cpu_state.d,
+        cpu_state.e,
+        cpu_state.h,
+        cpu_state.l,
+        cpu_state.sp,
+        cpu_state.pc,
+        cpu_state.pcmem[0],
+        cpu_state.pcmem[1],
+        cpu_state.pcmem[2],
+        cpu_state.pcmem[3],
+    });
+}
