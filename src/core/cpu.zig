b8ef89a3af96fce444e77f80ae4038096eb3145f
Halfdan Sand Christensen <halfdansc@proton.me>
2026-03-31 11:32:51 +0200

    registers added

--- src/core/cpu.zig
+++ src/core/cpu.zig
@@ -1,13 +1,35 @@
-pub const CpuState = struct {
-    a: u8,
-    f: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    h: u8,
-    l: u8,
+const AF = packed union {
+    value: u16,
+    parts: packed struct { f: u8, a: u8 },
+};
+
+const BC = packed union {
+    value: u16,
+    parts: packed struct { c: u8, b: u8 },
+};
+
+const DE = packed union {
+    value: u16,
+    parts: packed struct { e: u8, d: u8 },
+};
+
+const HL = packed union {
+    value: u16,
+    parts: packed struct { l: u8, h: u8 },
+};
+
+pub const Registers = struct {
+    af: AF,
+    bc: BC,
+    de: DE,
+    hl: HL,
+    // stack pointer
     sp: u16,
+    // program counter
     pc: u16,
+};
+
+pub const CpuState = struct {
+    registers: Registers,
     pcmem: [4]u8,
 };
