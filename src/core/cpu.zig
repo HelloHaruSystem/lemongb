975983a82157fe5bbd2b2e002a2d1dbc06048be6
Halfdan Sand Christensen <halfdansc@proton.me>
2026-03-31 12:57:15 +0200

    register reset function

--- src/core/cpu.zig
+++ src/core/cpu.zig
@@ -29,6 +29,24 @@ pub const Registers = struct {
     pc: u16,
 };
 
+pub const Cpu = struct {
+    registers: Registers,
+    // IME: interupt master enable flag
+    ime: bool,
+
+    pub fn init() Cpu {}
+
+    fn resetRegisters(self: *Cpu) void {
+        // TODO: initial value for the f register depends on the checksum for now it is hardcodedas the non-zero checksum fix this when cartridge is implemented
+        self.registers.af.value = 0x01B0;
+        self.registers.bc.value = 0x0013;
+        self.registers.de.value = 0x00D8;
+        self.registers.hl.value = 0x014D;
+        self.registers.sp = 0xFFFE;
+        self.registers.pc = 0x0100;
+    }
+};
+
 pub const CpuState = struct {
     registers: Registers,
     pcmem: [4]u8,
