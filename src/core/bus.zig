f34bb6a61775ced33d70ca807c0d752edf4f6a70
Halfdan Sand Christensen <halfdansc@proton.me>
2026-03-31 13:46:34 +0200

    simple read

--- src/core/bus.zig
+++ src/core/bus.zig
@@ -13,4 +13,23 @@ pub const Bus = struct {
     // 0xFF80 - 0xFFFE: High RAM
     // 0xFFFF:          Interrupt Enable Register (IE)
     memory: [0xFFFF + 1]u8,
+
+    pub fn read(self: *Bus, address: u16) u8 {
+        // for now just return the data at the location
+        // in the future handle the regions differently
+        switch (address) {
+            0x0000...0x3FFF => return self.memory[address],
+            0x4000...0x7FFF => return self.memory[address],
+            0x8000...0x9FFF => return self.memory[address],
+            0xA000...0xBFFF => return self.memory[address],
+            0xC000...0xCFFF => return self.memory[address],
+            0xD000...0xDFFF => return self.memory[address],
+            0xE000...0xFDFF => return self.memory[address],
+            0xFE00...0xFE9F => return self.memory[address],
+            0xFEA0...0xFEFF => return self.memory[address],
+            0xFF00...0xFF7F => return self.memory[address],
+            0xFF80...0xFFFE => return self.memory[address],
+            0xFFFF => return self.memory[address],
+        }
+    }
 };
